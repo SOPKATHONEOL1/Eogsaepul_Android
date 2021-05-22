08016ae7e3081a8e5afce13517111113f509be00
teagh82 <teagh8237@gmail.com>
2021-05-23 06:13:19 +0900

    response and request 추가

--- app/src/main/java/org/sopt/eogsaepul/data/response/ResponseDetailData.kt
+++ app/src/main/java/org/sopt/eogsaepul/data/response/ResponseDetailData.kt
@@ -1,5 +1,17 @@
 package org.sopt.eogsaepul.data.response
 
 data class ResponseDetailData(
-    val text : String
+    val status : Int,
+    val data : DetailData?
+)
+
+data class DetailData(
+    val image : String,
+    val reply : List<DetailDataList>
+)
+
+data class DetailDataList(
+    val replyText : String,
+    val replyIndex : Int,
+    val like : Int
 )
