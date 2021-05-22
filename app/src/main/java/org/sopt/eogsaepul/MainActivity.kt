debaa429221593912ffa093eee9f2eb1957091d2
ho-taek <kht072623@naver.com>
2021-05-23 07:10:15 +0900

    [FEAT] addSplash

--- app/src/main/java/org/sopt/eogsaepul/MainActivity.kt
+++ app/src/main/java/org/sopt/eogsaepul/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
